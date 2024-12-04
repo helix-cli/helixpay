b2b0d952b23f3e864c1c5aaf444b611ec55f5f54
misterbo271 <nevermind271999@gmail.com>
2024-12-04 18:37:55 +0700

    fix UI

--- screens/home/balance/index.tsx
+++ screens/home/balance/index.tsx
@@ -3,42 +3,21 @@ import styles from "./balance.module.css";
 import cn from "classnames";
 import ArrowRight from "@/components/Icons/ArrowRight";
 import Decorations from "@/components/Decorations";
+import CurrencyFormat from "@/components/CurrencyFormat";
 
 const Balance = () => {
   return (
     <section className={cn("section", styles.section)}>
       <div className={cn("container")}>
-        <Decorations />
 
         <div className={styles.content}>
-          <div className={styles.title_container}>
-            <h2 className={cn("heading-3", styles.title)}>
-             Balance.
-            </h2>
-            <p className={cn("paragraph-medium", styles.subtitle)}>
-              Get the tools to present your designs in their best light. Our
-              mockup collection has everything you need to impress clients and
-              audiences alike.
-            </p>
-            <button className={cn("button", styles.button)}>
-              Discover the collection
-              <div className={cn(styles.circle)}>
-                <ArrowRight />
-              </div>
-            </button>
-          </div>
-
-          <div className={styles.image_container}>
-            <Image
-              src="/images/macbooks.jpg"
-              layout="fill"
-              objectFit="cover"
-              alt="iphones"
+            <CurrencyFormat
+                className="text-h1 md:text-h3"
+                value={3200.8}
+                currency="$"
             />
-          </div>
         </div>
 
-        <Decorations />
       </div>
     </section>
   );
